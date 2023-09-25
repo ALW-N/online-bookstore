7d34a6173dd68433d34c0ef5e68d3f6322dae6f7
ALW-N <alwintomy11@gmail.com>
2023-09-25 19:15:27 +0530

    Commit-1(Book Search)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
+import { BsSearch } from 'react-icons/bs';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const productList = ["The Great Gatsby",
+"To Kill A Mockingbird",
+"The Alchemist",
+"Subtle art of Not Giving A "];
+	const [products, setProducts] = useState(productList);
+	const [searchVal, setSearchVal] = useState("");
+	function handleSearchClick() {
+		if (searchVal === "") { setProducts(productList); return; }
+		const filterBySearch = productList.filter((item) => {
+			if (item.toLowerCase()
+				.includes(searchVal.toLowerCase())) { return item; }
+		})
+		setProducts(filterBySearch);
+	}
+	const mystyle = {
+		marginLeft: "600px",
+		marginTop: "20px",
+		fontWeight: "700"
+	};
+
+	return (
+		<div>
+			<div style={mystyle}>
+				<input onChange={e => setSearchVal(e.target.value)}>
+				</input>
+				<BsSearch onClick={handleSearchClick} />
+			</div>
+			<div>
+
+				{products.map((product) => {
+					return (
+						<div style={mystyle}>{product}</div>
+					)
+				})
+				}
+
+			</div>
+		</div>
+	);
 }
 
 export default App;
